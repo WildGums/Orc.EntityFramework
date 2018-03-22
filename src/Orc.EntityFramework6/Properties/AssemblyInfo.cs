a58355902ab79f462c3aec539fc4a1e25d1a546d
Geert van Horrik <geert@catenalogic.com>
2018-03-22 16:17:41 +0100

    Migrate to new project system

---
+++ src/Orc.EntityFramework6/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="WildGums">
+//   Copyright (c) 2008 - 2018 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// All other assembly info is defined in SolutionAssemblyInfo.cs
+
+[assembly: AssemblyTitle("Orc.EntityFramework")]
+[assembly: AssemblyProduct("Orc.EntityFramework")]
+[assembly: AssemblyDescription("Orc.EntityFramework 6 library")]
+[assembly: NeutralResourcesLanguage("en-US")]
