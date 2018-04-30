62389d407de30303857bf856e25c78607830e628
Geert van Horrik <geert@catenalogic.com>
2018-04-30 20:56:34 +0200

    Migrate test project to new project structure

--- src/Orc.EntityFramework.Tests.Shared/PublicApiFacts.cs
+++ src/Orc.EntityFramework.Tests.Shared/PublicApiFacts.cs
@@ -9,11 +9,12 @@ namespace Orc.EntityFramework.Tests
 {
     using ApiApprover;
     using NUnit.Framework;
+    using System.Runtime.CompilerServices;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_EntityFramework_HasNoBreakingChanges()
         {
             var assembly = typeof(UnitOfWork).Assembly;
