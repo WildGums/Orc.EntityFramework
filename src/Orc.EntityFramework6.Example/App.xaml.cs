c45f66d90198caf8ba952bb316016963bb42a18e
Geert van Horrik <geert@catenalogic.com>
2026-08-21 13:25:07 +0200

    Update packages

--- src/Orc.EntityFramework6.Example/App.xaml.cs
+++ src/Orc.EntityFramework6.Example/App.xaml.cs
@@ -14,20 +14,18 @@
     {
         public App()
         {
-#if DEBUG
-            LogManager.AddDebugListener(false);
-#endif
+
         }
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            var languageService = ServiceLocator.Default.ResolveType<ILanguageService>();
+            //var languageService = ServiceLocator.Default.ResolveType<ILanguageService>();
 
-            // Note: it's best to use .CurrentUICulture in actual apps since it will use the preferred language
-            // of the user. But in order to demo multilingual features for devs (who mostly have en-US as .CurrentUICulture),
-            // we use .CurrentCulture for the sake of the demo
-            languageService.PreferredCulture = CultureInfo.CurrentCulture;
-            languageService.FallbackCulture = new CultureInfo("en-US");
+            //// Note: it's best to use .CurrentUICulture in actual apps since it will use the preferred language
+            //// of the user. But in order to demo multilingual features for devs (who mostly have en-US as .CurrentUICulture),
+            //// we use .CurrentCulture for the sake of the demo
+            //languageService.PreferredCulture = CultureInfo.CurrentCulture;
+            //languageService.FallbackCulture = new CultureInfo("en-US");
 
             base.OnStartup(e);
 
